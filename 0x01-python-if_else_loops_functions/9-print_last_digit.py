3266ee12a3f12b8f52747aecf230e5a4666ee93b
iolowookere217 <olowookere.isaaco@gmail.com>
2023-01-04 19:37:06 -0800

    Task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(n):
     if n < 0:
-        ld = (abs(n) % 10) * -1
-
-    else:
-        ld = n % 10
-
-        print(ld, end="")
-
+        n = -n
+    ld = n % 10
+    print(ld, end="")
     return ld
