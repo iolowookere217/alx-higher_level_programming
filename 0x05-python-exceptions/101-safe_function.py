4c36d385535c6799f291ffdde0daa4756c422594
iolowookere217 <olowookere.isaaco@gmail.com>
2023-01-25 15:51:03 -0800

    Advanced tasks

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def safe_function(fct, *args):
+
+    from sys import stderr
+
+    try:
+
+        f = fct(*args)
+
+        return (f)
+
+    except Exception as err:
+
+        print("Exception: {}".format(err), file=stderr)
+
+        return None
