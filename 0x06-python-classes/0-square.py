153580625d53525df748958c14d00630975fb945
iolowookere217 <olowookere.isaaco@gmail.com>
2023-01-25 15:09:17 -0800

    Task 0 to 6

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"0-square.py define"
+
+
+class Square:
+
+    """An empty class that defines a square
+
+    """
+
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"0-square.py define"
+
+
+class Square:
+
+    """A class that defines a square
+
+    """
+
+    def __init__(self, size):
+
+        """Def
+
+        """
+
+        self.__size = size
