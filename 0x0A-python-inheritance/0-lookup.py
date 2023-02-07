ba73fd0032af840826b9d78d184c2cc1072ca0a1
iolowookere217 <olowookere.isaaco@gmail.com>
2023-02-06 18:55:27 -0800

    All task

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Lookup
+"""
+
+
+def lookup(obj):
+
+    """Function that returns the list of avail. attributes and methods
+    of an object.
+    """
+
+    return dir(obj)
