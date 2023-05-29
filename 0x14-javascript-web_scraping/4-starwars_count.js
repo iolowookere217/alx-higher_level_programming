c0a3f0a278a77701829f3eda73586c108d452f2e
iolowookere217 <olowookere.isaaco@gmail.com>
2023-05-29 19:52:44 +0000

    task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+const actor = 'https://swapi-api.alx-tools.com/api/people/18/';
+
+request(url, { json: true }, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const films = body.results;
+    const match = films.filter((film) => film.characters.includes(actor));
+    console.log(match.length);
+  }
+});
