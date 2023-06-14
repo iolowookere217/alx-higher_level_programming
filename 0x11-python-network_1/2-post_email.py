ae8e58aecb6fec250a11dfc085ad325f3367d285
iolowookere217 <olowookere.isaaco@gmail.com>
2023-06-14 10:52:31 +0000

    task 1 2 and 3 updated

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""a Python script that takes in a URL and an email,
+    sends a POST request to the passed URL with
+    the email as a parameter,and displays the body of the
+    response (decoded in utf-8) ascii is equivalent to utf8"""
+
+import sys
+import urllib.parse as parse
+from urllib import request
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        url = sys.argv[1]
+        values = {'email': sys.argv[2]}
+
+        data = parse.urlencode(values)
+        data = data.encode('ascii')
+        req = request.Request(url, data)
+
+        with request.urlopen(url) as response:
+            the_page = response.read()
+            print(the_page.decode("utf-8"))
---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" a Python script that takes in a URL, sends a request to the URL
+and displays the body of the response (decoded in utf-8)."""
+
+import sys
+import urllib.request as request
+
+
+if __name__ == '__main__':
+    try:
+        if len(sys.argv) > 1:
+            url = sys.argv[1]
+            with request.urlopen(url) as response:
+                print(response.read().decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
